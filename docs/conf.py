d6ceca584c44e2817ffdbca5ace959c859dbb21f
Emilia Killer <emilliaartemiskiller@gmail.com>
2023-02-20 14:15:01 +0200

    Auto documented using Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -2,27 +2,25 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
 import django
 
+sys.path.insert(0, os.path.abspath(".."))
+os.environ["DJANGO_SETTINGS_MODULE"] = "capstone.settings"
+django.setup()
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "capstone-site"
 copyright = "2023, Emilia Killer"
 author = "Emilia Killer"
-release = "0.01"
+release = "0.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-sys.path.insert(0, os.path.abspath(".."))
-os.environ["DJANGO_SETTINGS_MODULE"] = "capstone.settings"
-django.setup()
-
-extensions = []
+extensions = ["sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
